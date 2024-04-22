25482d5e63efd5d1173d214b141b836fc17479fd
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-22 19:05:18 +0600

    function parameter

---
+++ parameter.js
@@ -0,0 +1,25 @@
+function vatKhao() {
+  console.log("hat duya aso");
+  console.log("boso");
+  console.log("plata khabar dabar nao");
+  console.log("gopat gopat kora khao");
+}
+
+// vatKhao();
+
+const number = 4;
+
+function square(n) {
+  console.log(n);
+  const borgo = n * n;
+  console.log(borgo);
+}
+
+// square(number);
+
+function add(n1, n2) {
+  const sum = n1 + n2;
+  console.log(sum);
+}
+
+add(1, 2);
