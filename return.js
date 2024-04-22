dea0d6a83297ca1fa62ded8b625c6c859e4a9998
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-22 19:43:39 +0600

    function return

---
+++ return.js
@@ -0,0 +1,15 @@
+function TenTimes(number) {
+  const result = number * 10;
+  return result;
+}
+
+function cutHalf(n) {
+  const half = n / 2;
+  return half;
+}
+
+const output = TenTimes(5);
+const result = cutHalf(100);
+
+console.log(output);
+console.log(result);
