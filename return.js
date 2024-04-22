73904ff57ff052b4796cf2e260d36d99ee8b2e29
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-22 23:44:23 +0600

    conditional function

--- return.js
+++ return.js
@@ -11,5 +11,17 @@ function cutHalf(n) {
 const output = TenTimes(5);
 const result = cutHalf(100);
 
-console.log(output);
-console.log(result);
+function evenOdd(number) {
+  if (number % 2 === 0) {
+    return "Even number";
+  } else if (number % 2 === 1) {
+    return "Odd number";
+  }
+}
+
+// console.log(output);
+// console.log(result);
+
+const res = evenOdd(12);
+
+console.log(res);
