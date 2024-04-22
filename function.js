2ac8d7794cdf1b7ac757cea764c3b2884b4ed25d
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-22 12:28:34 +0600

    create function && call function

--- function.js
+++ function.js
@@ -1,1 +1,9 @@
 const number = 45;
+
+function fanOfKor() {
+  console.log("bosa thaka uta dara");
+  console.log("hattya hattya switch ar kasa jao");
+  console.log("fan ar switch click koro");
+}
+
+fanOfKor();
