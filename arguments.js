fe501dd57fd2c524986bc6d74ff926ee1e67b022
hamimDev <mdhamimhowladerasif@gmail.com>
2024-04-22 19:25:00 +0600

    function arguments

---
+++ arguments.js
@@ -0,0 +1,20 @@
+function doubleIt(number) {
+  const doubled = number * 2;
+  console.log(number, doubled);
+}
+
+// doubleIt(25);
+// doubleIt(15);
+// doubleIt(35);
+// doubleIt(4*5);
+
+function difference(n1, n2) {
+  const dif = n1 - n2;
+  console.log(dif);
+}
+
+const fatherAge = 53;
+
+const myAge = 21;
+
+difference(fatherAge, myAge);
